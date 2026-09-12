a9603e12becedaae7f0091336223e4ebc19f9b4d
Zachary Maillard <zmaillard@protonmail.com>
2026-09-12 14:25:31 -0600

    Initial Commit

---
+++ tides/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tides
+//
+//  Created by Zach Maillard on 9/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tides/tidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  tidesApp.swift
+//  tides
+//
+//  Created by Zach Maillard on 9/12/26.
+//
+
+import SwiftUI
+
+@main
+struct tidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
