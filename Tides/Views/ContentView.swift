61727773909fbc7a78802ca4378bc6ffca3ccf24
Zachary Maillard <zmaillard@protonmail.com>
2026-09-20 17:01:29 -0600

    feat: navigation and initial ci

--- Tides/Views/ContentView.swift
+++ Tides/Views/ContentView.swift
@@ -9,47 +9,24 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) var modelContext
-    @AppStorage("StationId") private var stationId = ""
+    @State private var navigator = Navigator()
     
-    @Query private var station: [Station]
-    @State var forecastViewModel = ForecastViewModel()
-    
-    init() {
-        let predicate = #Predicate<Station> { station in
-            station.id == stationId
-        }
-        
-        _station = Query(filter: predicate)
-    }
     
     var body: some View {
-        VStack {
-            Text(stationId)
-            if !station.isEmpty {
-                Text(station[0].name)
-            }
-            switch forecastViewModel.state {
-            case .idle:
-                Text("No data yet")
-            case .loading:
-                ProgressView {
-                    Text("Loading...")
-                }.navigationTitle("Loading Forecast")
-            case .loaded(let predictions):
-                List(predictions){ pred in
-                    PredictionListItem(prediction: pred)
-                }.navigationTitle(forecastViewModel.title)
-            case .error(let error):
-                Text(error).foregroundStyle(Color.red)
-            }
-        }
-        .padding()
-        .task {
-            if !station.isEmpty {
-                await forecastViewModel.fetch(for: station[0])
-            }
-        }
+        @Bindable var navigator = navigator
+        NavigationStack(path: $navigator.path) {
+            PredictionView()
+                .navigationDestination(for: Screen.self) { route in
+                    switch route {
+                    case .root:
+                        PredictionView()
+                    case .settings:
+                        SettingsView(viewModel: SettingsViewModel())
+                    }
+                }
+        }.onOpenURL{ url in
+            navigator.handleDeepLink(url)
+        }.environment(navigator)
     }
 }
 
